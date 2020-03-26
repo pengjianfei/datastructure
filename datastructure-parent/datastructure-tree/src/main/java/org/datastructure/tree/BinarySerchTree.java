701e36abe3b5e080ea1cff24d9ac4ad831216760
helloall <1172606755@qq.com>
2020-03-26 22:56:54 +0800

    删除app.java 添加BinarySerchTree.java

---
+++ datastructure-parent/datastructure-tree/src/main/java/org/datastructure/tree/BinarySerchTree.java
@@ -0,0 +1,53 @@
+package org.datastructure.tree;
+
+public class BinarySerchTree<E> {
+	
+	/***
+	 * s元素个数
+	 * @return
+	 */
+    public int size() {
+		return 0;
+	}
+    /**
+     * 是否为空树
+     * @return
+     */
+    boolean isEmpty() {
+    	return true;
+    }
+    
+	/**
+	 * 清空列表
+	 */
+	public void clear() {
+
+	}
+
+	/**
+	 * 增加元素
+	 * 
+	 * @param e
+	 */
+	public void add(E element) {
+    	
+    }
+    
+    /***
+     * 删除某个元素
+     * @param element
+     */
+    public void remove(E element) {
+    	
+    }
+    
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(E element) {
+    	return false;
+    }
+
+}
