6a3d9e485966458477384ac976cde1dc429e6178
helloall <1172606755@qq.com>
2020-03-26 22:39:19 +0800

    init data structure 2020306

---
+++ datastructure-parent/datastructure-tree/src/main/java/org/datastructure/tree/App.java
@@ -0,0 +1,13 @@
+package org.datastructure.tree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
